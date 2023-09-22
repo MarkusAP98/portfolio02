e5953bdf5811567eff68e86fa8a39beed6cb5977
Markus Phantha <markus.a.phantha@gmail.com>
2023-09-22 15:20:30 +0300

    added video

--- src/components/Main/main.js
+++ src/components/Main/main.js
@@ -1,9 +1,24 @@
-import React from 'react'
-import './main.css'
-const main = () => {
-  return (
-    <div>main</div>
-  )
-}
+import React from "react";
+import "./main.css";
+import video from "../../assets/videos/backgroundVid.mp4";
 
-export default main
+const Main = () => {
+  return (
+    <div className="main">
+       <div className="video-background">
+      <video autoPlay loop muted>
+        <source
+          src={video}
+          type="video/mp4"
+        />
+        Your browser does not support the video tag.
+      </video>
+      {/* Add your content on top of the video */}
+      <div className="content">{/* Your content goes here */}</div>
+    </div>
+    </div>
+   
+  );
+};
+
+export default Main;
