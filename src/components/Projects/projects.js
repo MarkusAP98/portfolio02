d617fe868e1d95be137b40dcc369d6ec247ac798
Markus Phantha <markus.a.phantha@gmail.com>
2023-09-23 17:04:18 +0300

    added image and some elements

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -1,10 +1,20 @@
-import React from 'react'
-import './projects.css'
+import React from "react";
+import "./projects.css";
+import BarplayClone from "../../assets/images/barplay_clone.png";
 
 const projects = () => {
   return (
-    <div>projects</div>
-  )
-}
+    <div className="projects">
+    <h1>Projects</h1>
+      <div className="projects__wrapper">
+        <div className="barplay">
+          <a href="#">
+            <img src={BarplayClone} className="barplay_img"></img>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default projects
+export default projects;
