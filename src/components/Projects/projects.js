6af19a312e0f2972197a84ce3287818e50bf80f7
Markus Phantha <markus.a.phantha@gmail.com>
2023-09-25 16:27:36 +0300

    added component for chat_gpt3

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -2,24 +2,45 @@ import React from "react";
 import "./projects.css";
 import BarplayClone from "../../assets/images/barplayClone02.png";
 
+
 const projects = () => {
   return (
     <div className="projects">
       <h1>PROJECTS 👩🏻‍💻</h1>
       <div className="projects__wrapper">
-        <div className="barplay">
+        <div className="allProjects">
           <a href="#">
-            <img src={BarplayClone} className="barplay_img"></img>
+            <img src={BarplayClone} className="allProjects_img"></img>
           </a>
-          <div className="barplay_text">
-            <h3>Barplay Clone</h3>
+          <div className="allProjects_text">
+            <h3>Barplay</h3>
             <p>
               Barplay is a very popular bar in joensuu. They have this nice
-              clean design so i had to change myself to build a similar one.
+              clean design so I had to challenge myself to build a similar one.
               This website purpose is to let the costumer know when the bar is
               open and what service food or drinks they offer.
             </p>
-            <div className="barplay_btns">
+            <div className="allProjects_btns">
+              <button>View Code</button>
+              <button>Live Website</button>
+            </div>
+          </div>
+        </div>
+
+        {/* --------UUno-------- */}
+        <div className="allProjects" style={{ flexDirection: "row-reverse" }}>
+          <a href="#">
+            <img src={BarplayClone} className="allProjects_img"></img>
+          </a>
+          <div className="allProjects_text">
+            <h3>Personal Trainer</h3>
+            <p>
+              Barplay is a very popular bar in joensuu. They have this nice
+              clean design so I had to challenge myself to build a similar one.
+              This website purpose is to let the costumer know when the bar is
+              open and what service food or drinks they offer.
+            </p>
+            <div className="allProjects_btns">
               <button>View Code</button>
               <button>Live Website</button>
             </div>
