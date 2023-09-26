9f412bc540960b0bf91236926a05728d4b5e5316
Markus Phantha <markus.a.phantha@gmail.com>
2023-09-26 17:48:05 +0300

    footer elements

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,10 +1,23 @@
-import React from 'react'
-import './footer.css'
+import React from "react";
+import "./footer.css";
 
 const footer = () => {
   return (
-    <div>footer</div>
-  )
-}
+    <div className="footer">
+      <div>
+        <h1>Markus Arunchai Phantha</h1>
+        <p>
+          A Frontend focused Web Developer building the Frontend of Websites
+        </p>
+      </div>
+      <div>
+        <h1>Social</h1>
+        <ul>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default footer
+export default footer;
