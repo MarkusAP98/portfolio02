99f040260c887b8ca57129f064c44a21a59e8ddf
Markus Phantha <markus.a.phantha@gmail.com>
2023-09-27 16:01:20 +0300

    added info and social icons to footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,20 +1,35 @@
 import React from "react";
 import "./footer.css";
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 
 const footer = () => {
   return (
     <div className="footer">
-      <div>
-        <h1>Markus Arunchai Phantha</h1>
-        <p>
-          A Frontend focused Web Developer building the Frontend of Websites
-        </p>
+      <div className="footer-info">
+        <div className="footer_text">
+          <h1>Markus Arunchai Phantha</h1>
+          <p>
+            A Frontend focused Web Developer building the Frontend of Websites
+          </p>
+        </div>
+        <div className="footer_icons">
+          <h1>Social</h1>
+          <ul>
+            <li>
+              <a>
+                <AiFillGithub />
+              </a>
+            </li>
+            <li>
+              <a>
+                <AiFillLinkedin />
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div>
-        <h1>Social</h1>
-        <ul>
-          <li></li>
-        </ul>
+      <div className="footer-copyRight">
+        <p>© CopyRight 2023. Made by Markus Phantha</p>
       </div>
     </div>
   );
